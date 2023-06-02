40bdfc5424d0a2b4d8b22e3722c1ce7d29ee84bf
Ivan Uribe <ivanmauriciour@ufps.edu.co>
2023-06-02 17:59:20 -0500

    DELETE import forms for html forms

--- books/views.py
+++ books/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect
-from .forms import SearchBookForm
 # Create your views here.
 
 ERROR_MESSAGE_FORM = "If you want to search for books you need the title or name of the author."
 
 def index(request):
-    search_form = SearchBookForm()
-    return render(request, 'books/index.html', {'search_form' : search_form})
+    return render(request, 'books/index.html', {})
 
 def get_book(request):
     if not request.POST['search_title'] or not request.POST['search_author']:
