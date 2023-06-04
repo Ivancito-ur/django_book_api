b5fabd32f4f8db2b5f1eaf8a06a468fd6b4c19a9
Ivan Uribe <ivanmauriciour@ufps.edu.co>
2023-06-03 22:36:35 -0500

    ADD new methods for save, delete and show favorites books

--- books/urls.py
+++ books/urls.py
@@ -5,5 +5,8 @@ app_name = 'books'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get_books', views.get_books_by_title_or_author, name='get_books_title_author')
+    path('get_books', views.get_books_by_title_or_author, name='get_books_title_author'),
+    path('save_favorite_book/<str:id_book>', views.save_favorite_book, name='save_favorite_book'),
+    path('get_favorite_books', views.get_favorite_books, name='get_favorite_books'),
+    path('delete_favorite_book/<str:id_book>', views.delete_favorite_book, name='delete_favorite_book'),
 ]
